4d42eef9eef564025cb1901ca378eee15a0990c3
Amaal Ali <amq.ali@gmail.com>
2016-11-14 22:25:15 +0000

    Resturant model defined.

--- spec/features/restaurants_feature_spec.rb
+++ spec/features/restaurants_feature_spec.rb
@@ -8,4 +8,13 @@ feature 'restaurants' do
       expect(page).to have_link('Add a restaurant')
     end
   end
+
+  context 'restaurants have been added' do
+    scenario 'restaurant is displayed' do
+      Restaurant.create(name: 'Frank Doubles')
+      visit '/restaurants'
+      expect(page).to have_content('Frank Doubles')
+      expect(page).not_to have_content('No restaurants yet')
+    end
+  end
 end
