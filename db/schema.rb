ecd9b901f5c68dc47c8b139525e9ff507dc7fa06
Christopher Sanchez <sanchez.christopherm@gmail.com>
2016-11-17 14:45:59 +0100

    Stage 4 of v2 complete Hallelujah

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161117101553) do
+ActiveRecord::Schema.define(version: 20161117132523) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
